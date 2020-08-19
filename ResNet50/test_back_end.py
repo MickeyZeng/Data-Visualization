8454f4e6e2dfed61fef8491cf3867bfc8e94b21b
jakecastelli <959672929@qq.com>
2020-08-19 11:12:02 +0930

    comment line to prevent bug

--- ResNet50/test_back_end.py
+++ ResNet50/test_back_end.py
@@ -19,7 +19,8 @@ import ResNet50.get_neural_network as gnn
 import ResNet50.json_to_dict as jtd
 import demoConnect.util as du
 
-# matplotlib.use('agg')
+import matplotlib
+matplotlib.use('agg')
 
 numOfResult = 0
 rank = 0
